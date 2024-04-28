de3d42903fa33dced9137d994e5aca2d732d1a04
Dustin Feldt <dfeldt5@uw.edu>
2024-04-28 00:35:03 -0700

    Work on QuestionPanel and MazePanel

--- TriviaMaze/src/view/QuestionPanel.java
+++ TriviaMaze/src/view/QuestionPanel.java
@@ -5,8 +5,11 @@ import model.Question;
 import javax.swing.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
+import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.util.ArrayList;
 
 public class QuestionPanel extends JPanel implements PropertyChangeListener, ChangeListener {
 
@@ -17,21 +20,26 @@ public class QuestionPanel extends JPanel implements PropertyChangeListener, Cha
 
     private JLabel myQuestionBody;
 
-    private ButtonGroup myAnswerButtons;
-
     private Question myQuestion;
 
     public QuestionPanel() {
         super();
         myQuestionBody = new JLabel();
-        myAnswerButtons = new ButtonGroup();
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-        setComponents();
+        add(myQuestionBody);
+        createButtons();
     }
 
-    private void setComponents() {
+    private void createButtons() {
+        final char[] answers = {'A', 'B', 'C', 'D'};
 
+        for (char c : answers) {
+            final JButton button = new JButton(new AnswerAction(c));
+            button.setAlignmentX(Component.CENTER_ALIGNMENT);
+            add(button);
+        }
     }
+
 
     /**
      * This method gets called when a bound property is changed.
@@ -52,5 +60,25 @@ public class QuestionPanel extends JPanel implements PropertyChangeListener, Cha
     @Override
     public void stateChanged(final ChangeEvent theEvent) {
 
+    }
+
+    private final class AnswerAction extends AbstractAction{
+
+        private final char answerChar;
+        AnswerAction(final char theAnswer) {
+            super(theAnswer + ") ");
+            answerChar = theAnswer;
+            putValue(Action.SHORT_DESCRIPTION, "Answer " + answerChar);
+        }
+
+        /**
+         * Invoked when an action occurs.
+         *
+         * @param theEvent the event to be processed
+         */
+        @Override
+        public void actionPerformed(ActionEvent theEvent) {
+            firePropertyChange("answer chosen", null, answerChar);
+        }
     }
 }
