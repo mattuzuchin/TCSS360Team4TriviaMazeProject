6df4a344eea9f537552eb64d6c4ee26abfc24064
Matthew Uzunoe-Chin <mattuzuchin@gmail.com>
2024-05-05 14:16:55 -0700

    new file:   TriviaMaze/src/Model/Maze.java

---
+++ TriviaMaze/src/Model/Room.java
@@ -0,0 +1,41 @@
+package Model;
+
+
+import java.awt.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Room {
+
+    private final Point myCoordinates;
+
+    private final Map<Direction, Door> myDoors;
+
+    private final int mySize;
+
+    public Room(final Point theCoordinates, final int theSize) {
+        myCoordinates = theCoordinates;
+        myDoors = new HashMap<>();
+        mySize = theSize;
+        setDoors();
+    }
+
+    private void setDoors() {
+        if (myCoordinates.getY() != 0) {
+            myDoors.put(Direction.NORTH, new Door());
+        }
+        if (myCoordinates.getX() != 0) {
+            myDoors.put(Direction.WEST, new Door());
+        }
+        if (myCoordinates.getY() != mySize) {
+            myDoors.put(Direction.SOUTH, new Door());
+        }
+        if (myCoordinates.getX() != mySize) {
+            myDoors.put(Direction.EAST, new Door());
+        }
+    }
+
+    public Map<Direction, Door> getDoors() {
+        return myDoors;
+    }
+}
---
+++ TriviaMaze/src/View/TriviaMazeMain.java
@@ -0,0 +1,11 @@
+//package View;
+//
+//import java.awt.*;
+//
+//public class TriviaMazeMain {
+//    private TriviaMazeMain() {}
+//
+//    public static void main(final String[] theArgs) {
+//        EventQueue.invokeLater(() -> new TriviaMazeGUI());
+//    }
+//}
