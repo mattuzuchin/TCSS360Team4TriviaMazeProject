30215bf9f5d76a78132712e7314a790c27b42086
Dustin <103460016+DFeldt5@users.noreply.github.com>
2024-06-04 00:31:32 -0700

    Update Room.java

--- TriviaMaze/src/Model/Room.java
+++ TriviaMaze/src/Model/Room.java
@@ -3,22 +3,64 @@ package Model;
 
 import java.io.Serializable;
 
+/**
+ * @author Matthew Uzunoe-Chin, Elias Arriola, Dustin Feldt
+ * @version Spring 2024
+ * Implementation of a Room.
+ */
 public class Room implements Serializable {
-    private Doors myDoors;
-    private int myRow;
-    private int myColumn;
+    /**
+     * Field represents the rooms Doors.
+     */
+    private final Doors myDoors;
+    /**
+     * Field represents the rooms row location.
+     */
+    private final int myRow;
+    /**
+     * Field represents the rooms column location.
+     */
+    private final int myColumn;
 
+    /**
+     * Constructor for the Room.
+     * @param theRow set row
+     * @param theColumn set col
+     */
     public Room(final int theRow, final int theColumn) {
         myDoors = new Doors();
         myRow = theRow;
         myColumn = theColumn;
     }
 
+    /**
+     *
+     * @return the rooms row.
+     */
+    public int getRow() {
+        return myRow;
+    }
 
+    /**
+     *
+     * @return the room's column.
+     */
+    public int getColumn() {
+        return myColumn;
+    }
+
+    /**
+     *
+     * @return the rooms Doors.
+     */
     public Doors getDoors() {
         return myDoors;
     }
 
+    /**
+     *
+     * @return String representation of the Rooms state.
+     */
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
